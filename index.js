532d51a317bff9e20182cc7de3ccf6aa8dde467f
Sergey <smisyuk@gmail.com>
2023-03-01 22:41:42 +0200

    refactoring code

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-
-import { getWeather } from "./weather.js"
-
+import got from 'got';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.static('public'))
 
+const BASE_URL = 'http://api.weatherapi.com/v1/current.json'
 
+app.get('/api/weather/:country', async (req, res) => {
+  const country = req.params.country
 
-app.get('/weather/:country', async (req, res) => {
-    const country = req.params.country
-    try{
-      const data = await getWeather(country)
-      console.log("data in index.js: ", data)
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-    
+  try{
+    const response = await got(BASE_URL, {
+      searchParams:{
+        key: process.env.KEY,
+        q: country,
+      }
+    }).json()
+
+    const {
+          current: { last_updated, temp_c, wind_kph},          
+          location: { name },
+        } = response
+
+    res.status(200).json({ name, last_updated, temp_c, wind_kph});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }    
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`http://localhost:${process.env.PORT}/weather/`),
+  console.log(`http://localhost:${process.env.PORT}/api/weather/`),
 );
